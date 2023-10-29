f8eba1172d7f5d03ac7a76cc51ae951ec3fdecb8
Werefox <jack@wrightus.com>
2023-10-29 16:47:53 -0400

    Changed how the router was implemented to be in the App component instead of index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      APP
+      <Header />  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<p>APP</p>}/>
+          <Route path='/about' element={<p>about</p>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,22 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>
-  }
-])
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
